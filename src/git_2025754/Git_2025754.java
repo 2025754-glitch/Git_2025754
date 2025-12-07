e045ab6fcc592b927d6be954ecc70e3ddc97c378
2025754-glitch <2025754@student.cct.ie>
2025-12-07 01:40:20 +0000

    initial commit

---
+++ src/git_2025754/Git_2025754.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025754;
+
+public class Git_2025754 {
+
+    public static void main(String[] args) {
+        System.out.println("Grazielle - 2025754");
+    }
+}
